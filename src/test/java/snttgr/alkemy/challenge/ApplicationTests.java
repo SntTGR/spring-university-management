f82014c0c99b22b9cc6e287546137f7ead9ca953
Santiago Nobili <sanobili@hotmail.com>
2021-03-03 19:19:37 -0300

    setup commit

---
+++ src/test/java/snttgr/alkemy/challenge/ApplicationTests.java
@@ -0,0 +1,13 @@
+package snttgr.alkemy.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
